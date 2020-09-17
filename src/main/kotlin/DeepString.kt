f6adfd5ef5b0f413769762aea75d7fe97c24a6da
ruckustboom <uberawesomeemailaddressofdoom@gmail.com>
2020-09-17 09:30:31 -0600

    Make accessors more Java friendly

--- src/main/kotlin/DeepString.kt
+++ src/main/kotlin/DeepString.kt
@@ -9,12 +9,33 @@ public sealed class DeepString<S : Any>(public val value: S) {
         other as DeepString<*>
         return value == other.value
     }
+
+    public companion object {
+        @JvmStatic
+        public fun deep(value: Any?): DeepString<*>? = deep.deep(value)
+
+        @JvmStatic
+        public fun deep(vararg values: Any?): DeepStringList = deep.deep(*values)
+
+        @JvmStatic
+        public fun deep(vararg entries: Pair<*, *>): DeepStringMap = deep.deep(*entries)
+
+        @JvmStatic
+        public fun get(value: DeepString<*>?, index: Int): DeepString<*>? = value[index]
+
+        @JvmStatic
+        public fun get(value: DeepString<*>?, key: String): DeepString<*>? = value[key]
+
+        @JvmStatic
+        public fun containsKey(value: DeepString<*>?, key: String): Boolean = key in value
+    }
 }
 
 public class DeepStringMap(value: Map<String, DeepString<*>?>) : DeepString<Map<String, DeepString<*>?>>(value)
 public class DeepStringList(value: List<DeepString<*>?>) : DeepString<List<DeepString<*>?>>(value)
 public class DeepStringValue(value: String) : DeepString<String>(value)
 
+@JvmSynthetic
 public fun deep(value: Any?): DeepString<*>? = when (value) {
     null -> null
     is DeepString<*> -> value
@@ -36,10 +57,30 @@ public fun deep(value: Any?): DeepString<*>? = when (value) {
     else -> DeepStringValue(value.toString())
 }
 
-public fun deep(vararg entries: Pair<*, *>): DeepStringMap = DeepStringMap(entries.associate { (k, v) -> k.toString() to deep(v) })
+@JvmSynthetic
+public fun deep(vararg entries: Pair<*, *>): DeepStringMap =
+    DeepStringMap(entries.associate { (k, v) -> k.toString() to deep(v) })
+
+@JvmSynthetic
 public fun deep(vararg values: Any?): DeepStringList = DeepStringList(values.map(::deep))
 
-public fun DeepString<*>?.get(): String = (this as DeepStringValue).value
-public operator fun DeepString<*>?.get(index: Int): DeepString<*>? = (this as DeepStringList).value[index]
-public operator fun DeepString<*>?.get(key: String): DeepString<*>? = (this as DeepStringMap).value[key]
-public operator fun DeepString<*>?.contains(key: String): Boolean = key in (this as DeepStringMap).value
+@JvmSynthetic
+public operator fun DeepString<*>?.get(index: Int): DeepString<*>? = (this as? DeepStringList)?.value?.getOrNull(index)
+
+@JvmSynthetic
+public operator fun DeepString<*>?.get(key: String): DeepString<*>? = (this as? DeepStringMap)?.value?.get(key)
+
+@JvmSynthetic
+public operator fun DeepString<*>?.contains(key: String): Boolean = this is DeepStringMap && key in value
+
+public val DeepString<*>?.string: String?
+    @JvmSynthetic
+    get() = (this as? DeepStringValue)?.value
+
+public val DeepString<*>?.list: List<DeepString<*>?>?
+    @JvmSynthetic
+    get() = (this as? DeepStringList)?.value
+
+public val DeepString<*>?.map: Map<String, DeepString<*>?>?
+    @JvmSynthetic
+    get() = (this as? DeepStringMap)?.value
