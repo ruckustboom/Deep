4fcaa50bf20425b97d0fa18490d712f6f24cb3e3
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2021-11-26 10:15:50 -0700

    Extract map to extension

--- src/main/kotlin/Deep.kt
+++ src/main/kotlin/Deep.kt
@@ -1,13 +1,10 @@
 package deep
 
-public sealed class Deep<out T> {
-    public abstract fun <R> map(convert: (T) -> Deep<R>): Deep<R>
-}
+public sealed class Deep<out T>
 
 public class DeepMap<out T>(public val data: Map<String, Deep<T>>) : Deep<T>() {
     public constructor(vararg entries: Pair<String, Deep<T>>) : this(entries.toMap())
 
-    override fun <R> map(convert: (T) -> Deep<R>): Deep<R> = DeepMap(data.mapValues { (_, it) -> it.map(convert) })
     override fun toString(): String = data.toString()
     override fun hashCode(): Int = data.hashCode()
     override fun equals(other: Any?): Boolean {
@@ -21,7 +18,6 @@ public class DeepMap<out T>(public val data: Map<String, Deep<T>>) : Deep<T>() {
 public class DeepList<out T>(public val data: List<Deep<T>>) : Deep<T>() {
     public constructor(vararg elements: Deep<T>) : this(elements.toList())
 
-    override fun <R> map(convert: (T) -> Deep<R>): Deep<R> = DeepList(data.map { it.map(convert) })
     override fun toString(): String = data.toString()
     override fun hashCode(): Int = data.hashCode()
     override fun equals(other: Any?): Boolean {
@@ -33,7 +29,6 @@ public class DeepList<out T>(public val data: List<Deep<T>>) : Deep<T>() {
 }
 
 public class DeepValue<out T>(public val data: T) : Deep<T>() {
-    override fun <R> map(convert: (T) -> Deep<R>): Deep<R> = convert(data)
     override fun toString(): String = data.toString()
     override fun hashCode(): Int = data?.hashCode() ?: 0
     override fun equals(other: Any?): Boolean {
@@ -42,6 +37,14 @@ public class DeepValue<out T>(public val data: T) : Deep<T>() {
         other as DeepValue<*>
         return data == other.data
     }
+}
+
+// Common Helpers
+
+public fun <T, R> Deep<T>.map(convert: (T) -> Deep<R>): Deep<R> = when(this) {
+    is DeepMap -> DeepMap(data.mapValues { (_, it) -> it.map(convert) })
+    is DeepList -> DeepList(data.map { it.map(convert) })
+    is DeepValue -> convert(data)
 }
 
 public fun <T> Deep<Deep<T>>.flatten(): Deep<T> = map { it }
