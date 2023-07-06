dd0c125e0a22a44c54c3466c757712849bd45595
Ruckus T-Boom <uberawesomeemailaddressofdoom@gmail.com>
2023-07-06 15:23:26 -0600

    Kotlin 1.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.0"
+    kotlin("jvm") version "1.9.0"
     id("maven-publish")
 }
 
@@ -27,7 +27,7 @@ tasks.test {
 }
 
 tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "19"
+    kotlinOptions.jvmTarget = "20"
     kotlinOptions.freeCompilerArgs += "-Xjvm-default=all"
 }
 
